25576ff5b4837901b3fbbc1f261d6c2ea41b6b19
Noah Huppert <contact@noahh.io>
2018-07-05 23:03:17 -0400

    added code to start grpc server

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
 import "fmt"
+import "net"
+import "log"
+
+import "github.com/Noah-Huppert/time-tracker/config"
+import "github.com/Noah-Huppert/time-tracker/users"
+
+import "google.golang.org/grpc"
 
 func main() {
-	fmt.Println("vim-go")
+	// Get config
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatalf("error loading configuration: %s\n", err.Error())
+	}
+
+	// Initialize RPC
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.GRPCPort))
+	if err != nil {
+		log.Fatalf("error listening on GRPC port: %s\n", err.Error())
+	}
+
+	grpcServer := grpc.NewServer()
+	users.RegisterUsersServer(grpcServer, &users.DefaultUsersServer{})
+	grpcServer.Serve(listener)
 }
