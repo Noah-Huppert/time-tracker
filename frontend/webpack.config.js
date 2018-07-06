e5968a734d4fa041eb47f12ef0a2d591f9f7c38b
Noah Huppert <contact@noahh.io>
2018-07-05 23:50:34 -0400

    configured webpack for es6

---
+++ frontend/webpack.config.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+	entry: "./js/main.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "main.bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: "babel-loader",
+				query: {
+					presets: ["es2015"]
+				}
+			}
+		]
+	},
+	stats: {
+		colors: true
+	},
+	devtool: "source-map"
+};
