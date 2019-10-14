c659e51ef2140543f4aaecef2c29a02736c029f9
Noah Huppert <contact@noahh.io>
2019-10-13 21:15:08 -0400

    app: started working on timer widget

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'dart:async';
 
 final GoogleSignIn _googleSignIn = GoogleSignIn();
 final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -14,12 +15,65 @@ class MyApp extends StatelessWidget {
       title: 'Time Tracker',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Time Tracker'),
+          title: Text('Time Tracker'),
         ),
         body: Center(
-          child: Text("Track time."),
+          child: Container(
+            child: TimerWidget(),
+          ),
         ),
       ),
     );
   }
+}
+
+class TimerWidgetState extends State<TimerWidget> {
+  Timer timer;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Center(
+          child: _buildTopWidget(),
+        ),
+        Row(
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () {
+                // Start timer if not running
+                if (timer == null || !timer.isActive) {
+                  setState(() {
+                    timer = Timer.periodic(Duration(seconds: 1), (timer){});
+                  });
+                } else { // Stop timer if running
+                  setState(() {
+                      timer.cancel();
+                  });
+                }
+              },
+              child: _buildToggleWidget(),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTopWidget() {
+    return Text("TOP WIDGET");
+  }
+
+  Widget _buildToggleWidget() {
+    if (timer != null && timer.isActive) {
+      return Text("Stop");
+    } else {
+      return Text("Start");
+    }
+  }
+}
+
+class TimerWidget extends StatefulWidget {
+  @override
+  TimerWidgetState createState() => TimerWidgetState();
 }
