3910670b261012a66447a231250b62a51eed496d
Noah Huppert <contact@noahh.io>
2020-01-07 01:59:48 -0500

    removed mostly old files

--- api/db-migrations/2_create_client_table.up.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE client (
-	  id SERIAL PRIMARY KEY,
-	  name TEXT NOT NULL
-)
--- api/db-migrations/3_create_project_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE project (
-	  id SERIAL PRIMARY KEY,
-	  client_id INTEGER
-	  		  NOT NULL
-	  		  REFERENCES client
-			  ON DELETE CASCADE,
-	  name TEXT NOT NULL,
-	  description TEXT
-)
