2c1ca7e8aa84eaf3a83405c5eda55df9467bbf24
Noah Huppert <contact@noahh.io>
2020-01-07 15:23:43 -0500

    created basic API server & db schema setup

--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ go 1.13
 
 require (
 	github.com/Noah-Huppert/goconf v0.2.2
-	github.com/Noah-Huppert/gointerrupt v0.1.0
+	github.com/Noah-Huppert/gointerrupt v1.0.0
 	github.com/Noah-Huppert/golog v1.2.0
+	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
 	github.com/golang-migrate/migrate/v4 v4.7.1
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
@@ -13,5 +14,5 @@ require (
 	github.com/sqs/rego v0.0.0-20191222034614-1e638b3db7d1 // indirect
 	golang.org/x/crypto v0.0.0-20191227163750-53104e6ec876 // indirect
 	golang.org/x/sys v0.0.0-20200106162015-b016eb3dc98e // indirect
-	golang.org/x/tools v0.0.0-20200106190116-7be0a674c9fc // indirect
+	golang.org/x/tools v0.0.0-20200107050322-53017a39ae36 // indirect
 )
--- main.go
+++ main.go
@@ -2,8 +2,12 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
+	"net/http"
 	"os"
+	"sync"
 
 	"github.com/Noah-Huppert/goconf"
 	"github.com/Noah-Huppert/gointerrupt"
@@ -20,12 +24,56 @@ type Config struct {
 	// HTTPAddr is the address for the HTTP server
 	HTTPAddr string `default:":8000" validate:"required"`
 
-	// DBURI is the database URI
-	DBURI string `default:"postgres://localhost:5432/dev-time-tracker" validate:"required"`
+	// DB is database configuration
+	DB struct {
+		// Address of server
+		Address string `default:"localhost:5432" validate:"required"`
+
+		// Database
+		Database string `default:"dev-time-tracker" validate:"required"`
+
+		// User
+		User string `default:"dev-time-tracker" validate:"required"`
+
+		// Password
+		Password string `default:"dev-time-tracker" validate:"required"`
+
+		// Options are all the connection options formated as key=value
+		// delimited by ampersands.
+		// Example: one=value&two=value
+		Options string `default:"sslmode=disable"`
+	}
+}
+
+// DBURI returns a postgres URI with all the .DB parameters
+func (c Config) DBURI(redact bool) string {
+	optsStr := ""
+	if len(c.DB.Options) > 0 {
+		optsStr = fmt.Sprintf("?%s", c.DB.Options)
+	}
+
+	return fmt.Sprintf("postgres://%s:%s@%s/%s%s", c.Redact(redact, c.DB.User),
+		c.Redact(redact, c.DB.Password),
+		c.DB.Address, c.DB.Database, optsStr)
+}
+
+// Redact erases a sensative value if the redact arg is true, it ensures
+// redaction keeps intacted if the value was empty or not for debugging.
+// If the redact arg is false raw is returned.
+func (c Config) Redact(redact bool, raw string) string {
+	if redact {
+		if len(raw) == 0 {
+			return "REDACTED_EMPTY"
+		} else {
+			return "REDACTED_NOT_EMPTY"
+		}
+	} else {
+		return raw
+	}
 }
 
 func main() {
-	ctx, _ := gointerrupt.NewCtx()
+	ctxPair := gointerrupt.NewCtxPair(context.Background())
 	log := golog.NewLogger("api")
 
 	// Load config
@@ -37,27 +85,59 @@ func main() {
 	}
 
 	// Connect to database
-	db, err := sql.Open("postgres", cfg.DBURI)
+	db, err := sql.Open("postgres", cfg.DBURI(false))
 	if err != nil {
-		log.Fatalf("failed to open db connection: %s", err)
+		log.Fatalf("failed to open db connection to \"%s\": %s",
+			cfg.DBURI(true), err)
 	}
 
-	if err := db.PingContext(ctx); err != nil {
-		log.Fatalf("failed to ping db: %s", err)
+	if err := db.PingContext(ctxPair.Graceful()); err != nil {
+		log.Fatalf("failed to ping db \"%s\": %s", cfg.DBURI(true), err)
 	}
 
 	// Determine what to do
-	switch os.Args[0] {
+	cmdArg := ""
+	if len(os.Args) >= 2 {
+		cmdArg = os.Args[1]
+	}
+
+	switch cmdArg {
 	case "server":
-		// API server
+		var wg sync.WaitGroup
+
+		// API routes
 		e := echo.New()
+		e.GET("/health", func(c echo.Context) error {
+			c.JSON(http.StatusOK, map[string]interface{}{
+				"ok": true,
+			})
+			return nil
+		})
+
+		// Start API server
 		log.Infof("starting HTTP server on %s", cfg.HTTPAddr)
 
-		if err := e.Start(cfg.HTTPAddr); err != nil {
-			log.Fatalf("failed to run HTTP server")
-		}
+		wg.Add(1)
+		go func() {
+			if err := e.Start(cfg.HTTPAddr); err != nil &&
+				err != http.ErrServerClosed {
+				log.Fatalf("failed to run HTTP server: %s", err)
+			}
+			wg.Done()
+		}()
+
+		// Gracefully shutdown server
+		go func() {
+			<-ctxPair.Graceful().Done()
+			if err := e.Shutdown(ctxPair.Harsh()); err != nil {
+				log.Fatalf("failed to shutdown HTTP server: %s", err)
+			}
+		}()
+
+		wg.Wait()
 		break
 	case "migrate":
+		// Setup migrator
 		dbDriver, err := migratePostgres.WithInstance(db,
 			&migratePostgres.Config{})
 		if err != nil {
@@ -70,9 +150,30 @@ func main() {
 			log.Fatalf("failed to create migrator: %s", err)
 		}
 
+		// Get pre migration status
+		version, dirty, err := migrator.Version()
+		if err != nil {
+			log.Fatalf("failed to get pre migration status: %s", err)
+		}
+
+		log.Debugf("pre migration status: version=%d, dirty=%t", version,
+			dirty)
+
+		// Run migrations
+		log.Infof("running database migrations")
+
 		if err := migrator.Up(); err != nil {
 			log.Fatalf("failed to migrate database: %s", err)
 		}
+
+		// Get post migration status
+		version, dirty, err = migrator.Version()
+		if err != nil {
+			log.Fatalf("failed to get post migration status: %s", err)
+		}
+
+		log.Debugf("post migration status: version=%d, dirty=%t", version,
+			dirty)
 		break
 	default:
 		log.Fatalf("command must be \"server\" or \"migrate\"")
