2b976289c8174caf4e7ea63c1b27978fc7030072
Noah Huppert <3323504+Noah-Huppert@users.noreply.github.com>
2023-08-11 00:00:36 -0400

    period rollup logic

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	golangLog "log"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 	"time"
 
@@ -37,9 +38,62 @@ func (e TimeEntry) Hash() (string, error) {
 }
 
 // TimeFormat represents YYYY-MM-DD HH:MM:SS
-const TimeFormat = "2006-1-2 15:04:05"
+const TimeFormat = "2006-01-02 15:04:05"
+
+// PeriodWeekly is a week duration
+const PeriodWeekly = time.Hour * 24
+
+// PeriodBiWeekly is a 2 week duration
+const PeriodBiWeekly = PeriodWeekly * 2
+
+// PeriodMonthly is a month duration
+const PeriodMonthly = PeriodBiWeekly * 2
+
+// PeriodWeeklyStr is the value a user must type to refer to PeriodWeekly
+const PeriodWeeklyStr = "weekly"
+
+// PeriodBiWeeklyStr is the value a user must type to refer to PeriodBiWeekly
+const PeriodBiWeeklyStr = "bi-weekly"
+
+// PeriodMonthlyStr is the value a user must type to refer to PeriodMonthly
+const PeriodMonthlyStr = "monthly"
+
+// ValidPeriodStrs is a list of all valid values a user could type to refer to a period
+var ValidPeriodStrs = []string{
+	PeriodWeeklyStr,
+	PeriodBiWeeklyStr,
+	PeriodMonthlyStr,
+}
+
+// ValidPeriodStrsJoined is a user friendly string listing values from ValidPeriodStrs
+var ValidPeriodStrJoined = strings.Join(ValidPeriodStrs, ", ")
+
+// ParsePeriod takes a str and converts it to a duration. See Period... constants
+func ParsePeriod(str string) (time.Duration, error) {
+	if str == PeriodWeeklyStr {
+		return PeriodWeekly, nil
+	} else if str == PeriodBiWeeklyStr {
+		return PeriodBiWeekly, nil
+	} else if str == PeriodMonthlyStr {
+		return PeriodMonthly, nil
+	}
+
+	return time.Hour * 0, fmt.Errorf("invalid period '%s', valid values: %s", str, ValidPeriodStrJoined)
+}
 
 func main() {
+	// Setup logger
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		golangLog.Fatalf("failed to initialize logger: %s\n", err)
+	}
+
+	defer func() {
+		if err := logger.Sync(); err != nil && !strings.Contains(err.Error(), "handle is invalid") {
+			golangLog.Fatalf("failed flush logger: %s\n", err)
+		}
+	}()
+
 	// Parse options
 	var inDir string
 	flag.StringVar(&inDir, "in-dir", "times", "Directory containing time tracking CSV files")
@@ -48,7 +102,7 @@ func main() {
 	flag.StringVar(&outDir, "out-dir", "reports", "Directory where reports for each billing period will be written")
 
 	var billingPeriod string
-	flag.StringVar(&billingPeriod, "billing-period", "bi-weekly", "How often bills will be issued, valid values: bi-weekly")
+	flag.StringVar(&billingPeriod, "billing-period", "bi-weekly", fmt.Sprintf("How often bills will be issued, valid values: %s", ValidPeriodStrJoined))
 
 	var columnStartTime string
 	flag.StringVar(&columnStartTime, "column-start-time", "time started", "Name of column which contains start time in format (24 hour time): YYYY-MM-DD HH:MM:SS")
@@ -61,17 +115,11 @@ func main() {
 
 	flag.Parse()
 
-	// Setup logger
-	logger, err := zap.NewDevelopment()
+	// Convert options into programmatic values
+	billingPeriodDuration, err := ParsePeriod(billingPeriod)
 	if err != nil {
-		golangLog.Fatalf("failed to initialize logger: %s\n", err)
+		logger.Fatal("failed to parse billing-period option", zap.Error(err))
 	}
-
-	defer func() {
-		if err := logger.Sync(); err != nil && !strings.Contains(err.Error(), "handle is invalid") {
-			golangLog.Fatalf("failed flush logger: %s\n", err)
-		}
-	}()
 
 	// Read in data
 	inFileNames, err := os.ReadDir(inDir)
@@ -147,5 +195,42 @@ func main() {
 		}
 	}
 
-	logger.Debug("time entries", zap.String("timeEntries", fmt.Sprintf("%#v", timeEntries)))
+	timeEntriesList := []TimeEntry{}
+	for _, entry := range timeEntries {
+		timeEntriesList = append(timeEntriesList, entry)
+	}
+
+	sort.Slice(timeEntriesList, func(i, j int) bool {
+		return timeEntriesList[i].StartTime.Before(timeEntriesList[j].StartTime)
+	})
+
+	if len(timeEntriesList) == 0 {
+		logger.Fatal("no time entries")
+	}
+
+	// Roll-up into reports
+	firstStartDate := timeEntriesList[0].StartTime
+
+	periodStart := time.Date(firstStartDate.Year(), firstStartDate.Month(), 1, 0, 0, 0, 0, firstStartDate.Location())
+	periodEnd := periodStart.Add(billingPeriodDuration)
+
+	billingPeriods := [][]time.Time{
+		{periodStart, periodEnd},
+	}
+	timeEntriesByPeriodStart := make(map[time.Time][]TimeEntry)
+	timeEntriesByPeriodStart[periodStart] = []TimeEntry{}
+
+	for _, entry := range timeEntriesList {
+		// Check if starting a new period
+		if entry.StartTime.After(periodEnd) {
+			periodStart = periodStart.Add(billingPeriodDuration)
+			periodEnd = periodEnd.Add(billingPeriodDuration)
+
+			billingPeriods = append(billingPeriods, []time.Time{periodStart, periodEnd})
+			timeEntriesByPeriodStart[periodStart] = []TimeEntry{}
+		}
+
+		// Add time entry
+		timeEntriesByPeriodStart[periodStart] = append(timeEntriesByPeriodStart[periodStart], entry)
+	}
 }
