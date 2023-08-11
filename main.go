e34433da90854ac617c4878b5b21ad262c7effbc
Noah Huppert <3323504+Noah-Huppert@users.noreply.github.com>
2023-08-11 02:27:35 -0400

    moved to struct methods, wrote outputs

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 
 	"github.com/mitchellh/hashstructure/v2"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 // TimeEntry records a period of time when work was completed
@@ -58,8 +59,11 @@ func (p BillingPeriod) Duration() time.Duration {
 	return total
 }
 
-// TimeFormat represents YYYY-MM-DD HH:MM:SS
-const TimeFormat = "2006-01-02 15:04:05"
+// InputTimeFormat represents YYYY-MM-DD HH:MM:SS
+const InputTimeFormat = "2006-01-02 15:04:05 MST"
+
+// FilePathTimeFormat is the format used in output file names
+const FilePathTimeFormat = "2006-01-02"
 
 // PeriodWeekly is a week duration
 const PeriodWeekly = time.Hour * 24 * 7
@@ -105,12 +109,19 @@ func ParsePeriod(str string) (time.Duration, error) {
 const OutputModesHelpStr = "print (Print to console), dir=<DIR> (Output CSVs to <DIR> directory)"
 
 type OutputMode interface {
-	Output(periods []BillingPeriod) error
+	Output(periods []BillingPeriod, csvCols TimeTrackerReportCSVColumns) error
+}
+
+type TimeTrackerReportCSVColumns struct {
+	ColumnStartTime string
+	ColumnEndTime   string
+	ColumnDuration  string
+	ColumnComment   string
 }
 
 type PrintOutputMode struct{}
 
-func (m PrintOutputMode) Output(periods []BillingPeriod) error {
+func (m PrintOutputMode) Output(periods []BillingPeriod, csvCols TimeTrackerReportCSVColumns) error {
 	for _, period := range periods {
 		fmt.Printf("\nPeriod: %s - %s - %s\n", period.StartTime, period.EndTime, period.Duration())
 		fmt.Println("============")
@@ -132,7 +143,104 @@ type DirOutputMode struct {
 	Dir string
 }
 
-func (m DirOutputMode) Output(periods []BillingPeriod) error {
+func (m DirOutputMode) writePeriodReport(period BillingPeriod, csvCols TimeTrackerReportCSVColumns) error {
+	// Open file
+	wd, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("failed to get working directory: %s", err)
+	}
+
+	outFile := filepath.Join(wd, m.Dir, fmt.Sprintf("bill-%s-%s.csv", period.StartTime.Format(FilePathTimeFormat), period.EndTime.Format(FilePathTimeFormat)))
+
+	f, err := os.OpenFile(outFile, os.O_CREATE|os.O_RDWR, 0660)
+	if err != nil {
+		return fmt.Errorf("failed to open report file '%s': %s", outFile, err)
+	}
+	writer := csv.NewWriter(f)
+
+	defer writer.Flush()
+
+	// Write header
+	err = writer.Write([]string{
+		csvCols.ColumnStartTime,
+		csvCols.ColumnEndTime,
+		csvCols.ColumnDuration,
+		csvCols.ColumnComment,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to write headers to report file '%s': %s", outFile, err)
+	}
+
+	// Write entries
+	for entryI, entry := range period.Entries {
+		err = writer.Write([]string{
+			entry.StartTime.String(),
+			entry.EndTime.String(),
+			entry.Duration().String(),
+			entry.Comment,
+		})
+		if err != nil {
+			return fmt.Errorf("failed to write entry %d to report file '%s': %s", entryI, outFile, err)
+		}
+	}
+
+	return nil
+}
+
+func (m DirOutputMode) writeRollup(periods []BillingPeriod, csvCols TimeTrackerReportCSVColumns) error {
+	// Open file
+	wd, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("failed to get working directory: %s", err)
+	}
+
+	outFile := filepath.Join(wd, m.Dir, "billing-periods.csv")
+
+	f, err := os.OpenFile(outFile, os.O_CREATE|os.O_RDWR, 0660)
+	if err != nil {
+		return fmt.Errorf("failed to open report file '%s': %s", outFile, err)
+	}
+	writer := csv.NewWriter(f)
+
+	defer writer.Flush()
+
+	// Write header
+	err = writer.Write([]string{
+		csvCols.ColumnStartTime,
+		csvCols.ColumnEndTime,
+		csvCols.ColumnDuration,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to write headers to report file '%s': %s", outFile, err)
+	}
+
+	for _, period := range periods {
+		err = writer.Write([]string{
+			period.StartTime.String(),
+			period.EndTime.String(),
+			period.Duration().String(),
+		})
+		if err != nil {
+			return fmt.Errorf("failed to write period %s-%s to report file '%s': %s", period.StartTime.String(), period.EndTime.String(), outFile, err)
+		}
+	}
+
+	return nil
+}
+
+func (m DirOutputMode) Output(periods []BillingPeriod, csvCols TimeTrackerReportCSVColumns) error {
+	// Record billing periods
+	for _, period := range periods {
+		if err := m.writePeriodReport(period, csvCols); err != nil {
+			return err
+		}
+	}
+
+	// Record rollup of periods
+	if err := m.writeRollup(periods, csvCols); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -146,32 +254,56 @@ func ParseOutputMode(str string) (OutputMode, error) {
 		return PrintOutputMode{}, nil
 	} else if matches := dirRegex.FindStringSubmatch(str); len(matches) > 0 {
 		return DirOutputMode{
-			Dir: matches[0],
+			Dir: matches[1],
 		}, nil
 	}
 
 	return nil, fmt.Errorf("string '%s' did not match any output modes, valid output modes: %s", str, OutputModesHelpStr)
 }
 
-func main() {
-	// Setup logger
+type TimeTracker struct {
+	logger *zap.Logger
+}
+
+type TimeTrackerReportOpts struct {
+	InDir           string
+	BillingPeriod   time.Duration
+	Timezone        string
+	ColumnStartTime string
+	ColumnEndTime   string
+	ColumnComment   string
+	OutputMode      OutputMode
+}
+
+func NewTimeTracker() *TimeTracker {
+	return &TimeTracker{}
+}
+
+func (t *TimeTracker) Init() {
 	logger, err := zap.NewDevelopment()
 	if err != nil {
 		golangLog.Fatalf("failed to initialize logger: %s\n", err)
 	}
 
-	defer func() {
-		if err := logger.Sync(); err != nil && !strings.Contains(err.Error(), "handle is invalid") {
-			golangLog.Fatalf("failed flush logger: %s\n", err)
-		}
-	}()
+	t.logger = logger
+}
 
+func (t *TimeTracker) Cleanup() {
+	if err := t.logger.Sync(); err != nil && !strings.Contains(err.Error(), "handle is invalid") {
+		golangLog.Fatalf("failed flush logger: %s\n", err)
+	}
+}
+
+func (t *TimeTracker) CLI() {
 	// Parse options
 	var inDir string
 	flag.StringVar(&inDir, "in-dir", "times", "Directory containing time tracking CSV files")
 
 	var billingPeriod string
 	flag.StringVar(&billingPeriod, "billing-period", PeriodBiWeeklyStr, fmt.Sprintf("How often bills will be issued, valid values: %s", ValidPeriodStrJoined))
+
+	var timeZone string
+	flag.StringVar(&timeZone, "timezone", "EST", "timezone in which input and output times are in")
 
 	var columnStartTime string
 	flag.StringVar(&columnStartTime, "column-start-time", "time started", "Name of column which contains start time in format (24 hour time): YYYY-MM-DD HH:MM:SS")
@@ -183,34 +315,143 @@ func main() {
 	flag.StringVar(&columnComment, "column-comment", "comment", "Name of column which contains an optional description of what happened during the time period")
 
 	var outputModeStr string
-	flag.StringVar(&outputModeStr, "output", "dir=report", fmt.Sprintf("Output mode, valid values: %s", OutputModesHelpStr))
+	flag.StringVar(&outputModeStr, "output", "dir=reports", fmt.Sprintf("Output mode, valid values: %s", OutputModesHelpStr))
 
 	flag.Parse()
 
 	// Convert options into programmatic values
 	billingPeriodDuration, err := ParsePeriod(billingPeriod)
 	if err != nil {
-		logger.Fatal("failed to parse billing-period option", zap.Error(err))
+		t.logger.Fatal("failed to parse billing-period option", zap.Error(err))
 	}
 
 	outputMode, err := ParseOutputMode(outputModeStr)
 	if err != nil {
-		logger.Fatal("failed to parse output-mode option", zap.Error(err))
+		t.logger.Fatal("failed to parse output-mode option", zap.Error(err))
 	}
 
+	// Run logic
+	opts := TimeTrackerReportOpts{
+		InDir:           inDir,
+		BillingPeriod:   billingPeriodDuration,
+		Timezone:        timeZone,
+		ColumnStartTime: columnStartTime,
+		ColumnEndTime:   columnEndTime,
+		ColumnComment:   columnComment,
+		OutputMode:      outputMode,
+	}
+
+	if err := t.Report(opts); err != nil {
+		t.logger.Fatal("failed to run report", zap.Any("error", err))
+	}
+}
+
+type InputDirectoryError struct {
+	InDir string
+	Err   error
+}
+
+func (e InputDirectoryError) MarshallLogObject(enc zapcore.ObjectEncoder) error {
+	enc.AddString("in-dir", e.InDir)
+	enc.AddString("error", e.Err.Error())
+	return nil
+}
+
+func (e InputDirectoryError) Error() string {
+	return e.Err.Error()
+}
+
+type InputFileError struct {
+	FilePath string
+	Err      error
+}
+
+func (e InputFileError) MarshallLogObject(enc zapcore.ObjectEncoder) error {
+	enc.AddString("file-path", e.FilePath)
+	enc.AddString("error", e.Err.Error())
+
+	return nil
+}
+
+func (e InputFileError) Error() string {
+	return e.Err.Error()
+}
+
+type MissingInputCSVColumnError struct {
+	FilePath   string
+	ColumnName string
+}
+
+func (e MissingInputCSVColumnError) MarshallLogObject(enc zapcore.ObjectEncoder) error {
+	enc.AddString("file-path", e.FilePath)
+	enc.AddString("error", e.Error())
+
+	return nil
+}
+
+func (e MissingInputCSVColumnError) Error() string {
+	return fmt.Sprintf("'%s' column not found in CSV file", e.ColumnName)
+}
+
+type InputCSVRowParseError struct {
+	FilePath      string
+	RowNum        int
+	ColumnName    string
+	ParseRawValue string
+	Err           error
+}
+
+func (e InputCSVRowParseError) MarshallLogObject(enc zapcore.ObjectEncoder) error {
+	enc.AddString("file-path", e.FilePath)
+	enc.AddInt("row", e.RowNum)
+	enc.AddString(e.ColumnName, e.ParseRawValue)
+	enc.AddString("error", e.Err.Error())
+
+	return nil
+}
+
+func (e InputCSVRowParseError) Error() string {
+	return e.Err.Error()
+}
+
+type InputCSVRowError struct {
+	FilePath string
+	RowNum   int
+	Err      error
+}
+
+func (e InputCSVRowError) MarshallLogObject(enc zapcore.ObjectEncoder) error {
+	enc.AddString("file-path", e.FilePath)
+	enc.AddInt("row", e.RowNum)
+	enc.AddString("error", e.Err.Error())
+
+	return nil
+}
+
+func (e InputCSVRowError) Error() string {
+	return e.Err.Error()
+}
+
+func (t *TimeTracker) Report(opts TimeTrackerReportOpts) error {
 	// Read in data
-	inFileNames, err := os.ReadDir(inDir)
+	inFileNames, err := os.ReadDir(opts.InDir)
 	if err != nil {
-		logger.Fatal("failed to read input directory", zap.Error(err), zap.String("in-dir", inDir))
+		return InputDirectoryError{
+			InDir: opts.InDir,
+			Err:   fmt.Errorf("failed to read input directory: %s", err),
+		}
 	}
 
 	timeEntries := make(map[string]TimeEntry) // keys are hashes of the values
 	for _, fileEntry := range inFileNames {
-		filePath := filepath.Join(inDir, fileEntry.Name())
+		filePath := filepath.Join(opts.InDir, fileEntry.Name())
 		// Open file
 		f, err := os.Open(filePath)
 		if err != nil {
-			logger.Fatal("failed to open input file", zap.Error(err), zap.String("file", filePath))
+			return InputFileError{
+				FilePath: filePath,
+				Err:      fmt.Errorf("failed to open input file: %s", err),
+			}
 		}
 
 		reader := csv.NewReader(f)
@@ -218,7 +459,10 @@ func main() {
 		// Read headers
 		headers, err := reader.Read()
 		if err != nil {
-			logger.Fatal("failed to read CSV headers", zap.Error(err), zap.String("file", filePath))
+			return InputFileError{
+				FilePath: filePath,
+				Err:      fmt.Errorf("failed to read CSV headers: %s", err),
+			}
 		}
 		headerMap := make(map[string]int)
 		for i, key := range headers {
@@ -226,47 +470,73 @@ func main() {
 		}
 
 		// Check for required columns
-		if _, ok := headerMap[columnStartTime]; !ok {
-			logger.Fatal("start time column not found in CSV file", zap.String("file", filePath), zap.String("column-start-time", columnStartTime))
+		if _, ok := headerMap[opts.ColumnStartTime]; !ok {
+			return MissingInputCSVColumnError{
+				FilePath:   filePath,
+				ColumnName: opts.ColumnStartTime,
+			}
 		}
 
-		if _, ok := headerMap[columnEndTime]; !ok {
-			logger.Fatal("end time column not found in CSV file", zap.String("file", filePath), zap.String("column-end-time", columnEndTime))
+		if _, ok := headerMap[opts.ColumnEndTime]; !ok {
+			return MissingInputCSVColumnError{
+				FilePath:   filePath,
+				ColumnName: opts.ColumnEndTime,
+			}
 		}
 
-		if _, ok := headerMap[columnComment]; !ok {
-			logger.Fatal("comment column not found in CSV file", zap.String("file", filePath), zap.String("column-comment", columnComment))
+		if _, ok := headerMap[opts.ColumnComment]; !ok {
+			return MissingInputCSVColumnError{
+				FilePath:   filePath,
+				ColumnName: opts.ColumnComment,
+			}
 		}
 
 		// Parse rows into TimeEntry structs
 		rows, err := reader.ReadAll()
 		if err != nil {
-			logger.Fatal("failed to read rows of CSV", zap.String("file", filePath), zap.Error(err))
+			return InputFileError{
+				FilePath: filePath,
+				Err:      fmt.Errorf("failed to read rows of CSV: %s", err),
+			}
 		}
 
 		for rowI, row := range rows {
 			// Parse date times
-			startTimeStr := row[headerMap[columnStartTime]]
-			startTime, err := time.Parse(TimeFormat, startTimeStr)
+			startTimeStr := fmt.Sprintf("%s %s", row[headerMap[opts.ColumnStartTime]], opts.Timezone)
+			startTime, err := time.Parse(InputTimeFormat, startTimeStr)
 			if err != nil {
-				logger.Fatal("failed to parse start time", zap.String("file", filePath), zap.Int("row", rowI), zap.Error(err), zap.String("raw start time", startTimeStr))
+				return InputCSVRowParseError{
+					FilePath:      filePath,
+					RowNum:        rowI,
+					ColumnName:    opts.ColumnStartTime,
+					ParseRawValue: startTimeStr,
+				}
 			}
 
-			endTimeStr := row[headerMap[columnEndTime]]
-			endTime, err := time.Parse(TimeFormat, endTimeStr)
+			endTimeStr := fmt.Sprintf("%s %s", row[headerMap[opts.ColumnEndTime]], opts.Timezone)
+			endTime, err := time.Parse(InputTimeFormat, endTimeStr)
 			if err != nil {
-				logger.Fatal("failed to parse end time", zap.String("file", filePath), zap.Int("row", rowI), zap.Error(err), zap.String("raw end time", endTimeStr))
+				return InputCSVRowParseError{
+					FilePath:      filePath,
+					RowNum:        rowI,
+					ColumnName:    opts.ColumnEndTime,
+					ParseRawValue: endTimeStr,
+				}
 			}
 
 			// Create entry
 			entry := TimeEntry{
 				StartTime: startTime,
 				EndTime:   endTime,
-				Comment:   row[headerMap[columnComment]],
+				Comment:   row[headerMap[opts.ColumnComment]],
 			}
 			entryHash, err := entry.Hash()
 			if err != nil {
-				logger.Fatal("failed to hash time entry", zap.String("file", filePath), zap.Int("row", rowI), zap.Error(err))
+				return InputCSVRowError{
+					FilePath: filePath,
+					RowNum:   rowI,
+					Err:      fmt.Errorf("failed to hash time entry: %s", err),
+				}
 			}
 			timeEntries[entryHash] = entry
 		}
@@ -282,14 +552,14 @@ func main() {
 	})
 
 	if len(timeEntriesList) == 0 {
-		logger.Fatal("no time entries")
+		return fmt.Errorf("no time entries")
 	}
 
 	// Roll-up into reports
 	firstStartDate := timeEntriesList[0].StartTime
 
 	periodStart := time.Date(firstStartDate.Year(), firstStartDate.Month(), 1, 0, 0, 0, 0, firstStartDate.Location())
-	periodEnd := periodStart.Add(billingPeriodDuration)
+	periodEnd := periodStart.Add(opts.BillingPeriod)
 
 	currentBillingPeriod := BillingPeriod{
 		StartTime: periodStart,
@@ -303,8 +573,8 @@ func main() {
 		if entry.StartTime.After(periodEnd) {
 			billingPeriods = append(billingPeriods, currentBillingPeriod)
 
-			periodStart = periodStart.Add(billingPeriodDuration)
-			periodEnd = periodEnd.Add(billingPeriodDuration)
+			periodStart = periodStart.Add(opts.BillingPeriod)
+			periodEnd = periodEnd.Add(opts.BillingPeriod)
 
 			currentBillingPeriod = BillingPeriod{
 				StartTime: periodStart,
@@ -320,8 +590,24 @@ func main() {
 	billingPeriods = append(billingPeriods, currentBillingPeriod)
 
 	// Output
-	logger.Debug("is", zap.String("outputMode", fmt.Sprintf("%#v", outputMode)))
-	if err := outputMode.Output(billingPeriods); err != nil {
-		logger.Fatal("failed to output reports", zap.Error(err))
+	reportCols := TimeTrackerReportCSVColumns{
+		ColumnStartTime: opts.ColumnStartTime,
+		ColumnEndTime:   opts.ColumnEndTime,
+		ColumnComment:   opts.ColumnComment,
+		ColumnDuration:  "duration",
 	}
+	if err := opts.OutputMode.Output(billingPeriods, reportCols); err != nil {
+		return fmt.Errorf("failed to output reports: %s", err)
+	}
+
+	return nil
+}
+
+func main() {
+	tracker := NewTimeTracker()
+	tracker.Init()
+
+	defer tracker.Cleanup()
+
+	tracker.CLI()
 }
