9530616fd22cd43cec4f6507c1a17c5778b933c4
Noah Huppert <contact@noahh.io>
2023-09-18 23:56:34 -0400

    frontend: made filter by date

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,52 +1,79 @@
 import { z } from "zod";
-import { Either, isLeft, left, right } from "fp-ts/lib/Either";
+import { Either, left, right } from "fp-ts/lib/Either";
 
 const timeEntrySchema = z.object({
   start_time: z.string(),
   end_time: z.string(),
   comment: z.string(),
   hash: z.string(),
-})
+});
 export type TimeEntrySchemaType = z.infer<typeof timeEntrySchema>;
 
-const BASE_URL = "http://localhost:4000/api/v0/"
+const BASE_URL = "http://localhost:4000/api/v0/";
 
 async function makeReq<T>({
-    path,
-    method,
-    shape,
-    body,
+  path,
+  method,
+  shape,
+  queryParams,
+  body,
 }: {
-    readonly path: string
-    readonly method: string
-    readonly shape: z.Schema<T>,
-    readonly body?: string,
+  readonly path: string;
+  readonly method: string;
+  readonly shape: z.Schema<T>;
+  readonly queryParams?: {[key: string]: string | undefined | null};
+  readonly body?: string;
 }): Promise<Either<Error, T>> {
-    // Make request
-    const res = await fetch(new URL(path, BASE_URL).href,
-    {
-        method,
-        body,
-    })
+  if (path[0] === "/") {
+    return left(new Error("path argument cannot start with leading slash, as this will clobber the base URL"));
+  }
 
-    // Decode response
-    const respBody = await res.json()
-    const decodeRes = shape.safeParse(respBody)
-    if (decodeRes.success === false) {
-      return left(new Error(`failed to parse body using schema: ${decodeRes.error}`));
-    }
+  // Make request
+  const setQueryParams: {[key: string]: string} = {}
+  if (queryParams !== undefined) {
+    Object.entries(queryParams).forEach(([key, value]) => {
+      if (value !== undefined && value !== null) {
+        setQueryParams[key] = value;
+      }
+    });
+  }
+  const url = new URL(path, BASE_URL).href + "?" + new URLSearchParams(setQueryParams || {})
+  const res = await fetch(url, {
+    method,
+    body,
+  });
 
-    return right(decodeRes.data);
+  // Decode response
+  const respBody = await res.json();
+  const decodeRes = shape.safeParse(respBody);
+  if (decodeRes.success === false) {
+    return left(
+      new Error(`failed to parse body using schema: ${decodeRes.error}`),
+    );
+  }
+
+  return right(decodeRes.data);
 }
 
 export const api = {
-    timeEntries: {
-        list: () => makeReq({
-            path: "time-entries",
-            method: "GET",
-            shape: z.object({
-                time_entries: z.array(timeEntrySchema),
-            }),
+  timeEntries: {
+    list: ({
+      startTime,
+      endTime,
+    }: {
+      readonly startTime?: Date
+      readonly endTime?: Date
+    }) =>
+      makeReq({
+        path: "time-entries",
+        method: "GET",
+        shape: z.object({
+          time_entries: z.array(timeEntrySchema),
         }),
-    }
-}
+        queryParams: {
+          "start_time": startTime?.toISOString(),
+          "end_time": endTime?.toISOString(),
+        }
+      }),
+  },
+};
--- frontend/src/pages/TimeEntries/TimeEntries.tsx
+++ frontend/src/pages/TimeEntries/TimeEntries.tsx
@@ -1,14 +1,33 @@
 import { useCallback, useEffect, useState } from "react";
 import { api, TimeEntrySchemaType } from "../../lib/api";
-import { CircularProgress, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
-import WarningIcon from '@mui/icons-material/Warning';
+import {
+  Button,
+  CircularProgress,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import WarningIcon from "@mui/icons-material/Warning";
 import { isLeft } from "fp-ts/lib/Either";
+import { DateTimePicker } from "@mui/x-date-pickers";
+
+type WebDataTimeEntriesList = TimeEntrySchemaType[] | "loading" | "error"
 
 export const PageTimeEntries = () => {
-  const [timeEntries, setTimeEntries] = useState<TimeEntrySchemaType[] | "loading" | "error">("loading")
+  const [filterStartTime, setFilterStartTime] = useState<Date | null>(null);
+  const [filterEndTime, setFilterEndTime] = useState<Date | null>(null);
+  const [timeEntries, setTimeEntries] = useState<WebDataTimeEntriesList>("loading");
 
   const fetchTimeEntries = useCallback(async () => {
-    const res = await api.timeEntries.list();
+    const res = await api.timeEntries.list({
+      startTime: filterStartTime,
+      endTime: filterEndTime,
+    });
+
     if (isLeft(res)) {
       console.error(`Failed to load time entries: ${res.left}`);
       setTimeEntries("error");
@@ -16,20 +35,70 @@ export const PageTimeEntries = () => {
     }
 
     setTimeEntries(res.right.time_entries);
-  }, [])
+  }, [filterStartTime, filterEndTime]);
 
   useEffect(() => {
-    fetchTimeEntries()
-  })
+    fetchTimeEntries();
+  }, [fetchTimeEntries]);
 
+  return (
+    <>
+      <DateFilter
+        label="Start Time"
+        value={filterStartTime}
+        onChange={setFilterStartTime}
+      />
+
+      <DateFilter
+        label="End Time"
+        value={filterEndTime}
+        onChange={setFilterEndTime}
+      />
+      <TimeEntriesTable timeEntries={timeEntries} />
+    </>
+  )
+};
+
+const DateFilter = ({
+  label,
+  value,
+  onChange,
+}: {
+  readonly label: string
+  readonly value: Date | null
+  readonly onChange: (date: Date | null) => void
+}) => {
+  return (
+    <>
+      <p>
+        {label}
+      </p>
+
+      <DateTimePicker
+        value={value}
+        onChange={onChange}
+      />
+
+      {value !== null && (
+        <Button onClick={() => onChange(null)}>
+          Clear
+        </Button>
+      )}
+    </>
+  )
+}
+
+const TimeEntriesTable = ({
+  timeEntries,
+}: {
+  readonly timeEntries: WebDataTimeEntriesList
+}) => {
   // If loading
   if (timeEntries === "loading") {
     return (
       <>
         <CircularProgress />
-        <p>
-          Loading time entries
-        </p>
+        <p>Loading time entries</p>
       </>
     );
   }
@@ -39,11 +108,9 @@ export const PageTimeEntries = () => {
     return (
       <>
         <WarningIcon />
-        <p>
-          Failed to load time entries
-        </p>
+        <p>Failed to load time entries</p>
       </>
-    )
+    );
   }
 
   // Show time entries table
@@ -52,35 +119,23 @@ export const PageTimeEntries = () => {
       <Table>
         <TableHead>
           <TableRow>
-            <TableCell>
-              Start Time
-            </TableCell>
+            <TableCell>Start Time</TableCell>
 
-            <TableCell>
-              End Time
-            </TableCell>
+            <TableCell>End Time</TableCell>
 
-            <TableCell>
-              Comment
-            </TableCell>
+            <TableCell>Comment</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {timeEntries.map((timeEntry) => (
             <TableRow key={timeEntry.hash}>
-              <TableCell>
-                {timeEntry.start_time}
-              </TableCell>
-              <TableCell>
-                {timeEntry.end_time}
-              </TableCell>
-              <TableCell>
-                {timeEntry.comment}
-              </TableCell>
+              <TableCell>{timeEntry.start_time}</TableCell>
+              <TableCell>{timeEntry.end_time}</TableCell>
+              <TableCell>{timeEntry.comment}</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
     </TableContainer>
   );
-};
+}
